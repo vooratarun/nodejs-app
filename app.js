4db474ca397c94e8f33226f7b23ab53e1f9dc41c
vooratarun <vooratarun@gmail.com>
2026-01-07 13:25:40 +0530

    trigger

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on Minikube!');
+  res.send('Hello from Node.js on Minikube! trigger');
 });
 
 app.get('/health', (req, res) => {
